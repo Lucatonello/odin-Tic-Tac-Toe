cb6c13f96476a58495777c8c89ea9be6d572ab05
Luca <lucatonello097@gmail.com>
2024-05-16 19:42:32 -0300

    working comand line version

--- script.js
+++ script.js
@@ -20,72 +20,52 @@ const player2 = {
     piece: 'o'
 };
 
+function checkWin(player) {
+    return (
+        (board[1] === player.piece && board[2] === player.piece && board[3] === player.piece) ||
+        (board[4] === player.piece && board[5] === player.piece && board[6] === player.piece) ||
+        (board[7] === player.piece && board[8] === player.piece && board[9] === player.piece) ||
+        (board[1] === player.piece && board[4] === player.piece && board[7] === player.piece) ||
+        (board[2] === player.piece && board[5] === player.piece && board[8] === player.piece) ||
+        (board[3] === player.piece && board[6] === player.piece && board[9] === player.piece) ||
+        (board[1] === player.piece && board[5] === player.piece && board[9] === player.piece) ||
+        (board[3] === player.piece && board[5] === player.piece && board[7] === player.piece)
+    );
+}
+
 function game () {
     let player1Wins = false;
     let player2Wins = false;
 
     while (player1Wins == false && player2Wins == false) {
+        var user1Choice, user2Choice;
         do {
-            var user1Choice = prompt("what square are you going player1?");
-            var user2Choice = prompt("what square are you going player2?");
-        } while (user1Choice > 0 && user1Choice <= 9 && user2Choice > 0 && user2Choice <= 9);
+            user1Choice = prompt("what square are you going player1?");
+        } while (user1Choice < 1 || user1Choice > 9 || board[user1Choice] !== ' ');
+
     
         board[user1Choice] = player1.piece;
+        console.log(board);
+
+        player1Wins = checkWin(player1);
+        if (player1Wins) {
+            console.log(`Tic Tac Toe! ${player1.name} wins!`);
+            break;
+        }
+
+        do {
+            user2Choice = prompt("what square are you going player2?");
+        }  while (user2Choice < 1 || user2Choice > 9 || board[user2Choice] !== ' ');
+
         board[user2Choice] = player2.piece;
+        console.log(board);
 
-        if (board[1] == player1.piece && board[2] == player1.piece && board[3] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[1] == player1.piece && board[4] == player1.piece && board[7] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[1] == player1.piece && board[5] == player1.piece && board[9] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[7] == player1.piece && board[8] == player1.piece && board[9] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[3] == player1.piece && board[6] == player1.piece && board[9] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[2] == player1.piece && board[5] == player1.piece && board[8] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[4] == player1.piece && board[5] == player1.piece && board[6] == player1.piece) {
-            player1Wins = true;   
-        }
-        else if (board[3] == player1.piece && board[5] == player1.piece && board[7] == player1.piece) {
-            player1Wins = true;   
+        player2Wins = checkWin(player2);
+        if (player2Wins) {
+            console.log(`Tic Tac Toe! ${player2.name} wins!`);
+            break;
         }
 
-
-        if (board[1] == player2.piece && board[2] == player2.piece && board[3] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[1] == player2.piece && board[4] == player2.piece && board[7] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[1] == player2.piece && board[5] == player2.piece && board[9] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[7] == player2.piece && board[8] == player2.piece && board[9] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[3] == player2.piece && board[6] == player2.piece && board[9] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[2] == player2.piece && board[5] == player2.piece && board[8] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[4] == player2.piece && board[5] == player2.piece && board[6] == player2.piece) {
-            player2Wins = true;   
-        }
-        else if (board[3] == player2.piece && board[5] == player2.piece && board[7] == player2.piece) {
-            player2Wins = true;   
-        }
-    }
-    if (player1Wins == true || player2Wins == true) {
-        console.log(`Tic Tac Toe! ${player1.name} wins!`);
-
-    }
+    } 
 };
+game();
